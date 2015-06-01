e8197be85002c7e51a49869e22bb3e016d7f4e96
Benedikt Kristinsson <benedikt@inventati.org>
2015-06-01 16:53:33 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# vcl
+
+This is a simple AST, Pretty Printer and Parser for VCL (the DSL for
+configuring Varnish).
+
+The [BNF can be found here](https://www.varnish-cache.org/trac/wiki/VCL.BNF).
+I have submitted an improvement that hasn't been added yet.
+[Here is a gist](https://gist.github.com/benediktkr/52d33ca982e29916a8aa/revisions)
+with my improvements. 
---
+++ vcl.hs
@@ -0,0 +1,29 @@
+import Data.Maybe
+import Data.List
+
+-- Non-terminals
+
+-- ACL ::= 'acl' identifier '{' {ACLENTRY} '}'
+data Acl = Acl Identifier [AclEntry]
+printAcl (Acl identifier entries) = "acl " ++ printIdentifier identifier ++ " {\n" ++ (intercalate "\n" (map printAclEntry entries)) ++ "\n}"
+
+-- ACLENTRY ::= ['!'] iprange ';' | '(' ['!'] iprange ')' ';' |  ['!'] '(' iprange ')' ';'
+data AclEntry = AclEntry IPRange (Maybe Bool)
+printAclEntry :: AclEntry -> String
+printAclEntry (AclEntry iprange Nothing)      = printIPRange iprange  ++ ";"
+printAclEntry (AclEntry iprange (Just True))  = printIPRange iprange ++ ";"
+printAclEntry (AclEntry iprange (Just False)) = "! " ++ printIPRange iprange ++ ";"
+
+---- Terminals
+-- For now I'm using strings to define terminals, but should be more precise 
+
+-- iprange ::= string [ '/' number ]
+data IPRange = IPRange String (Maybe Integer)
+printIPRange :: IPRange -> String
+printIPRange (IPRange iprange Nothing)     = iprange
+printIPRange (IPRange iprange (Just cidr)) = iprange ++ "/" ++ show cidr
+
+-- identifier ::= [a-zA-Z][a-zA-Z0-9_-]*
+data Identifier = Identifier String
+printIdentifier :: Identifier -> String
+printIdentifier (Identifier identifier) = identifier
